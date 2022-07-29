de0c5d02f9066c4d84bc53da19c70f40a3fb682d
Dexter Legaspi <1777690+dalegaspi@users.noreply.github.com>
2022-07-29 08:51:49 -0400

    removing files

--- src/main/java/edu/bu/cs665/hw03/entities/accounts/ClosableAnytimeAccount.java
+++
@@ -1,9 +0,0 @@
-package edu.bu.cs665.hw03.entities.accounts;
-
-/**
- * A marker interface to denote that an account can be closed anytime
- *
- * @author dlegaspi@bu.edu
- */
-public interface ClosableAnytimeAccount {
-}
--- src/main/java/edu/bu/cs665/hw03/misc/BankStatusView.java
+++
@@ -1,7 +0,0 @@
-package edu.bu.cs665.hw03.misc;
-
-public interface BankStatusView {
-    void println(String msg);
-
-    void print(String msg);
-}
